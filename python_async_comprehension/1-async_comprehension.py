fabf27058225bb47bed57a6d0db93e3f4a32556e
carlfrank <carl-frank7@hotmail.com>
2024-01-04 14:21:10 -0500

    changing 1 and 2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Module for async_comprehension coroutine"""
+"""Let's execute multiple coroutines at the same time with async"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-import asyncio  # Importing asyncio for asynchronous features
-# Importing async_generator from the previous task
-from 0-async_generator import async_generator  
 
-async def async_comprehension():
-    """A coroutine that collects 10 random numbers using async comprehension
-    over async_generator and returns them"""
-    return [i async for i in async_generator()]  # Async Comprehension
-
-# Rest of the code remains the same as in your 1-main.py file
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ return the list of all the delays (float values)."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delay_list = []
+    for future in asyncio.as_completed(tasks):
+        result = await future
+        delay_list.append(result)
+    return delay_list
