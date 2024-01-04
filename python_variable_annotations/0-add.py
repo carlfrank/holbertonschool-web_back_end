ebe8ff66ab5ddc7d9aaf7983a668c6f47af2f4c5
carlfrank <carl-frank7@hotmail.com>
2024-01-04 13:12:34 -0500

    0-add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-"""
-Module creates type annotated func that takes two floats arguments
-and returns their sum..
-"""
+"""Type-annotated function add float as arguments"""
 
 
-def add(a:float, b:float) -> float:
-    """function that returns the sum of a and b"""
+def add(a: float, b: float) -> float:
+    """Return sum of two floats as float"""
     return a + b
