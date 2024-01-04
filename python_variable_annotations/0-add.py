1a81dfa00d531973f3f860b5ba808d32c8a6d1bc
carlfrank <carl-frank7@hotmail.com>
2024-01-04 13:11:14 -0500

    0-add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,6 +4,7 @@ Module creates type annotated func that takes two floats arguments
 and returns their sum..
 """
 
+
 def add(a:float, b:float) -> float:
     """function that returns the sum of a and b"""
     return a + b
