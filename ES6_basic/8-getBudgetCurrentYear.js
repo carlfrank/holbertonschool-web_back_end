899e29803b7d82f9bda1f854c55bb9a52471e389
carlfrank <carl-frank7@hotmail.com>
2024-01-18 11:49:16 -0500

    ugly indent

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,10 +4,10 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {
+  const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
-    };
-    return budget;
+  };
+  return budget;
 }
