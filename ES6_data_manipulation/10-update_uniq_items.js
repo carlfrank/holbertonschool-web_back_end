f7cf7057bf8d3552c7dabf0b356941481d833249
carlfrank <carl-frank7@hotmail.com>
2024-01-31 11:44:53 -0500

    spaces update

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,13 +2,13 @@
 
 function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
+    throw new Error('Cannot process');
   }
 
   for (const [key, value] of map) {
-      if (value === 1) {
-          map.set(key, 100);
-      }
+    if (value === 1) {
+      map.set(key, 100);
+    }
   }
 
   return map;
