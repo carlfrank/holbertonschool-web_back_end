74bcff205fc1b851c2abd64155da568e4a993b24
carlfrank <carl-frank7@hotmail.com>
2024-01-31 11:15:17 -0500

    2 new spaces

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 function getListStudents() {
-const obj = [
+  const obj = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
