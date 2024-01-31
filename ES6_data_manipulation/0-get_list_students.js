c0131c95d800eb82a6600f65dfa4a55e3aa84dd1
carlfrank <carl-frank7@hotmail.com>
2024-01-31 11:14:03 -0500

    0-get list

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  const ids = students.map((student) => student.id);
-  return ids;
+function getListStudents() {
+const obj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return obj;
 }
-
-export default getListStudentIds;
+export default getListStudents;
