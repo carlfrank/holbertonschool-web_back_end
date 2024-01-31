1752de1dc8b9770e1cd016ebff549deb58ae0663
carlfrank <carl-frank7@hotmail.com>
2024-01-31 11:33:56 -0500

    5-typed arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-unction createInt8TypedArray(length, position, value) {
+function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer, 0);
 
